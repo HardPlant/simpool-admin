36398fada5af8bcd3b478c3249589581059194b6
HardPlant <abc7988se@naver.com>
2019-08-07 17:19:00 +0900

    Create 0.t_banner.sql

---
+++ sql/0.t_banner.sql
@@ -0,0 +1,10 @@
+CREATE TABLE T_DP_BNNR_MNG(
+    SUBJ            VARCHAR(50),
+    DP_YN           CHAR(1),
+    DP_STRT_DT      DATE,
+    DP_END_DT       DATE,
+    REGR            VARCHAR(50),
+    REG_DT          DATE,
+    BG_YN           CHAR(1),
+    BG_IMG_ADDR
+);
